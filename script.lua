13656063167b7792487f491d030aebc07e1fbf2f
K1ch1ro <99969988+K1ch1ro@users.noreply.github.com>
2022-11-19 14:14:37 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,8 +1,8 @@
 local DiscordLib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt")()
 
-local win = DiscordLib:Window("DarkLoader")
+local win = DiscordLib:Window("discord library")
 
-local serv = win:Server("Main", "")
+local serv = win:Server("Preview", "")
 
 local btns = serv:Channel("Buttons")
 
@@ -10,9 +10,25 @@ btns:Button("inf jump", function()
 loadstring(game:HttpGet("https://pastebin.com/raw/WweihvrM"))()
 DiscordLib:Notification("Notification", "inf jump", "Okay!")
 end)
-btns:Button("Set 100 speed", function()
-loadstring(game:HttpGet("https://pastebin.com/jfC9ZXLd"))()
-DiscordLib:Notification("Notification, "SpeedHack", "Okay!")
+
+btns:Button("speed", function()
+
+btns:Seperator()
+
+sldrs:Button("Change to 50", function()
+sldr:Change(50)
+end)
+
+local drops = serv:Channel("Dropdowns")
+
+local clrs = serv:Channel("Colorpickers")
+
+clrs:Colorpicker("ESP Color", Color3.fromRGB(255,1,1), function(t)
+print(t)
+end)
+
+local textbs = serv:Channel("Textboxes")
+
 local lbls = serv:Channel("Labels")
 
 lbls:Label("This is just a label.")
